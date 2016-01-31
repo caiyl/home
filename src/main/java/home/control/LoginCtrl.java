b227b3e30ff3a202ba24bbce0d6559fc70e3e4b4
caiyl <493199904@qq.com>
2016-01-31 22:40:53 +0800

    提交资源文件

---
+++ src/main/java/home/control/LoginCtrl.java
@@ -0,0 +1,31 @@
+package home.control;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import home.domain.User;
+
+@Controller
+public class LoginCtrl {
+	@RequestMapping("/login.do")
+    public String  login(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        System.out.println("hello--------------------------------");
+        return "/login";
+    }
+	
+	@RequestMapping(value="/loginIn.do", method=RequestMethod.POST)
+    public String loginIn(@ModelAttribute  User user) {
+        System.out.println(user.getUsername() + user.getPassword() );
+		return "/index";
+    }
+}
