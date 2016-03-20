accbed8268c36590ea9e4ec5fedcd1fd904197d8
caiyl <493199904@qq.com>
2016-03-20 17:31:45 +0800

    20160320

--- src/main/java/cn/itcast/f_sequenceFlow/SequenceFlowTest.java
+++ src/main/java/cn/itcast/f_sequenceFlow/SequenceFlowTest.java
@@ -1,31 +1,31 @@
-package cn.itcast.ProcessInstanceTest;
+package cn.itcast.f_sequenceFlow;
 
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
-import java.util.zip.ZipInputStream;
+import java.util.Map;
 
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngines;
-import org.activiti.engine.history.HistoricProcessInstance;
-import org.activiti.engine.history.HistoricTaskInstance;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.junit.Test;
 
-public class ProcessInstanceTest {
+public class SequenceFlowTest {
 
 	ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
 	
-	/**部署流程定义（从zip）*/
+	/**部署流程定义（从inputStream）*/
 	@Test
-	public void deploymentProcessDefinition_zip(){
-		InputStream in = this.getClass().getClassLoader().getResourceAsStream("diagrams/helloworld.zip");
-		ZipInputStream zipInputStream = new ZipInputStream(in);
+	public void deploymentProcessDefinition_inputStream(){
+		InputStream inputStreamBpmn = this.getClass().getResourceAsStream("sequenceFlow.bpmn");
+		InputStream inputStreamPng = this.getClass().getResourceAsStream("sequenceFlow.png");
 		Deployment deployment = processEngine.getRepositoryService()//与流程定义和部署对象相关的Service
 						.createDeployment()//创建一个部署对象
-						.name("流程定义")//添加部署的名称
-						.addZipInputStream(zipInputStream)//指定zip格式的文件完成部署
+						.name("连线")//添加部署的名称
+						.addInputStream("sequenceFlow.bpmn", inputStreamBpmn)//
+						.addInputStream("sequenceFlow.png", inputStreamPng)//
 						.deploy();//完成部署
 		System.out.println("部署ID："+deployment.getId());//
 		System.out.println("部署名称："+deployment.getName());//
@@ -35,7 +35,7 @@ public class ProcessInstanceTest {
 	@Test
 	public void startProcessInstance(){
 		//流程定义的key
-		String processDefinitionKey = "helloworld";
+		String processDefinitionKey = "sequenceFlow";
 		ProcessInstance pi = processEngine.getRuntimeService()//与正在执行的流程实例和执行对象相关的Service
 						.startProcessInstanceByKey(processDefinitionKey);//使用流程定义的key启动流程实例，key对应helloworld.bpmn文件中id的属性值，使用key值启动，默认是按照最新版本的流程定义启动
 		System.out.println("流程实例ID:"+pi.getId());//流程实例ID    101
@@ -45,7 +45,7 @@ public class ProcessInstanceTest {
 	/**查询当前人的个人任务*/
 	@Test
 	public void findMyPersonalTask(){
-		String assignee = "王五";
+		String assignee = "赵六";
 		List<Task> list = processEngine.getTaskService()//与正在执行的任务管理相关的Service
 						.createTaskQuery()//创建任务查询对象
 						/**查询条件（where部分）*/
@@ -79,52 +79,12 @@ public class ProcessInstanceTest {
 	@Test
 	public void completeMyPersonalTask(){
 		//任务ID
-		String taskId = "1404";
+		String taskId = "3103";
+		//完成任务的同时，设置流程变量，使用流程变量用来指定完成任务后，下一个连线，对应sequenceFlow.bpmn文件中${message=='不重要'}
+		Map<String, Object> variables = new HashMap<String, Object>();
+		variables.put("message", "重要");
 		processEngine.getTaskService()//与正在执行的任务管理相关的Service
-					.complete(taskId);
+					.complete(taskId,variables);
 		System.out.println("完成任务：任务ID："+taskId);
-	}
-	
-	/**查询流程状态（判断流程正在执行，还是结束）*/
-	@Test
-	public void isProcessEnd(){
-		String processInstanceId = "1001";
-		ProcessInstance pi = processEngine.getRuntimeService()//表示正在执行的流程实例和执行对象
-						.createProcessInstanceQuery()//创建流程实例查询
-						.processInstanceId(processInstanceId)//使用流程实例ID查询
-						.singleResult();
-		if(pi==null){
-			System.out.println("流程已经结束");
-		}
-		else{
-			System.out.println("流程没有结束");
-		}
-	}
-	
-	/**查询历史任务（后面讲）*/
-	@Test
-	public void findHistoryTask(){
-		String taskAssignee = "张三";
-		List<HistoricTaskInstance> list = processEngine.getHistoryService()//与历史数据（历史表）相关的Service
-						.createHistoricTaskInstanceQuery()//创建历史任务实例查询
-						.taskAssignee(taskAssignee)//指定历史任务的办理人
-						.list();
-		if(list!=null && list.size()>0){
-			for(HistoricTaskInstance hti:list){
-				System.out.println(hti.getId()+"    "+hti.getName()+"    "+hti.getProcessInstanceId()+"   "+hti.getStartTime()+"   "+hti.getEndTime()+"   "+hti.getDurationInMillis());
-				System.out.println("################################");
-			}
-		}
-	}
-	
-	/**查询历史流程实例（后面讲）*/
-	@Test
-	public void findHistoryProcessInstance(){
-		String processInstanceId = "1001";
-		HistoricProcessInstance hpi = processEngine.getHistoryService()//与历史数据（历史表）相关的Service
-						.createHistoricProcessInstanceQuery()//创建历史流程实例查询
-						.processInstanceId(processInstanceId)//使用流程实例ID查询
-						.singleResult();
-		System.out.println(hpi.getId()+"    "+hpi.getProcessDefinitionId()+"    "+hpi.getStartTime()+"    "+hpi.getEndTime()+"     "+hpi.getDurationInMillis());
 	}
 }
